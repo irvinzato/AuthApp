f4d4a099a87edf162a6afe8d1d50efb66e358340
irvinzato <multizato@hotmail.com>
2022-05-22 22:58:28 -0500

    Lazy load implementado en todos los modulos

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,7 +1,21 @@
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { MainComponent } from './pages/main/main.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent,  //No olvidar router-outltet para mostrar los sig. componentes
+    children: [
+      { path:'login', component:LoginComponent },
+      { path:'registro', component:RegisterComponent },
+      { path:'**', redirectTo:'login' }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/protected/protected-routing.module.ts
+++ src/app/protected/protected-routing.module.ts
@@ -1,7 +1,16 @@
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    children:[
+      { path:'', component:DashboardComponent },
+      { path:'**', redirectTo:'' }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
